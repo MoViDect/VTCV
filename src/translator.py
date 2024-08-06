b8f0c312a03dd4d6b792b7665da1179ac333dd8a
DongHwiCho <junejoe97@inha.edu>
2024-08-06 22:26:09 +0900

    [fix] double enter
    minor

--- src/translator.py
+++ src/translator.py
@@ -1,15 +1,24 @@
 from googletrans import Translator
 translator = Translator()
 
+
 def translate(text, src:str = 'en', dest:str = 'ko'):
     resp = text
     try:
         if len(text) >= 1:
             translated = translator.translate(str(text), src=src, dest=dest)
             resp = translated.text
-            print(resp)
+            # print(resp)
     except Exception as e:
         print(e, text)
+    return resp
+
+
+def translate_list(text_list, src:str = 'en', dest:str = 'ko'):
+    text_list = list(text_list)
+    translated = translator.translate(text_list, src=src, dest=dest)
+    # print(translated)
+    resp = [i.text for i in translated]
     return resp
 
 
@@ -21,5 +30,8 @@ if __name__ == '__main__':
     # 영어 = en
     # 러시아어 = ru
     # 스페인어 = es
-    translate('안녕하세요', 'ko', 'es')
-    translate('Hello', 'en', 'ru')
+    # translate('안녕하세요', 'ko', 'es')
+    # translate('Hello', 'en', 'ru')
+    list_trans = ['<4 / R >', "Texas Hold'em", '23', 'TEXAS', 'game of risk, luck, and skill:', '4', 'HOLDEM', '4.2', 'Times Played: 4', '2', 'Mahjong', "Texas Hold'em", 'President', 'Sevens', 'Last Card', 'Blackjack', 'Spee', 'Title Menu', 'Random Game', 'Change View']
+    translated = translator.translate(list_trans, src='en', dest='ko')
+    print([i.text for i in translated])
