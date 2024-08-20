0f6c49974f3d59fecbc3bba7b72d4c8ad72ad10d
D3SP3R4D0S <lee@inha.edu>
2024-08-20 20:30:11 +0900

    [fix] update translator

--- src/translator.py
+++ src/translator.py
@@ -1,8 +1,13 @@
 from googletrans import Translator
+from langchain_community.llms import Ollama
+import time
 translator = Translator()
 
 
+llm = Ollama(model="llama3.1")
+
 def translate(text, src:str = 'en', dest:str = 'ko'):
+    start_time = time.time()
     resp = text
     try:
         if len(text) >= 1:
@@ -11,6 +16,19 @@ def translate(text, src:str = 'en', dest:str = 'ko'):
             print(resp)
     except Exception as e:
         print(e, text)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"구글 실행 시간: {execution_time:.2f} 초")
+    return resp
+
+
+def ai_translate(text, src:str = 'en', dest:str = 'ko'):
+    start_time = time.time()
+    resp = llm.invoke(f"please translate \'{text}\' from {src} to {dest}. only translated result")
+    print(resp)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"AI 실행 시간: {execution_time:.2f} 초")
     return resp
 
 
@@ -22,5 +40,7 @@ if __name__ == '__main__':
     # 영어 = en
     # 러시아어 = ru
     # 스페인어 = es
-    translate('안녕하세요', 'ko', 'es')
-    translate('Hello', 'en', 'ru')
+    translate('안녕하세요 저는 한국사람 입니다 절 해치지 마세요', 'ko', 'es')
+    ai_translate('안녕하세요 저는 한국사람 입니다 절 해치지 마세요', 'ko', 'es')
+    translate('Hello I am kind korean please do not hurt me', 'en', 'ko')
+    ai_translate('Hello I am kind korean please do not hurt me', 'en', 'ko')
