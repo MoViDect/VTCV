007dc8d2cc9ce733021cdc03368d2af2d8080a31
DongHwiCho <junejoe97@inha.edu>
2024-11-19 01:26:29 +0900

    feat: 폰트 옵션에 따라서 폰트가 바뀔 수 있도록 쓰레딩 추가

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import cv2
+import threading
 from src.OCR import OCR
+from src.qtwindow import MyApp
+from PyQt5.QtWidgets import QApplication
+import sys
+from queue import Queue
 
-if __name__ == '__main__':
+    
+def main_work(q):
     # 캡쳐장비 불러오기 현재 자신의 상황에 맞춘 번호로 사용
+    font, engine = q.get()
+
     cap = cv2.VideoCapture(2)
 
     # 캡쳐장비 해상도 설정
@@ -15,7 +23,10 @@ if __name__ == '__main__':
         ret, frame = cap.read() 
 
         # OCR 실행 함수
-        ocr_frame = OCR(frame) 
+        if q.empty() is False:
+            font, engine = q.get()
+
+        ocr_frame = OCR(frame, font, engine) 
 
         # ocr_frame으로 받아온 이미지 출력
         cv2.imshow('frame', ocr_frame) 
@@ -24,8 +35,21 @@ if __name__ == '__main__':
         if cv2.waitKey(1) == 27:
             break
 
+
+if __name__ == '__main__':
+    data_queue = Queue()
+
+    # 창 실행 쓰레딩
+    # window_thread = threading.Thread(target=window_work, args=(app))
+    # window_thread.start()
+    app = QApplication(sys.argv)
+    ex = MyApp(data_queue)
+
+    main_thread = threading.Thread(target=main_work, args=(data_queue,))
+    main_thread.start()
+
     # 사용한 자원 해제 및 열려있는 창 종료
-    cap.release()  
+    sys.exit(app.exec_()) 
     cv2.destroyAllWindows()
 
         
